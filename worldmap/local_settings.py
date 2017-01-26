e848a99021e975024ee39ca9ae4e0d0481bbd233
capooti <pcorti@gmail.com>
2017-01-26 17:44:22 -0500

    Removed obsolete setting

--- worldmap/local_settings.py
+++ worldmap/local_settings.py
@@ -5,8 +5,6 @@ from django.conf import settings
 # GEONODE_CLIENT_LOCATION = '/static/worldmap_client/'
 GEONODE_CLIENT_LOCATION = "http://localhost:9090/"
 
-GEOSERVER_BASE_URL = "http://localhost:8080/geoserver/"
-
 #LAYER_PREVIEW_LIBRARY = 'geoext'
 LAYER_PREVIEW_LIBRARY = 'worldmap'
 
