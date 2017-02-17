b3a4bcc413d846d725ebec06bfd2dc7f4634441d
capooti <pcorti@gmail.com>
2017-02-17 14:52:58 -0500

    Sync with geonode master

--- wm_extra/migrations/0001_initial.py
+++ wm_extra/migrations/0001_initial.py
@@ -7,8 +7,8 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('layers', '26_initial'),
-        ('maps', '26_initial'),
+        ('layers', '24_initial'),
+        ('maps', '24_initial'),
     ]
 
     operations = [
