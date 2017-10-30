01078d035ff0c7659786a3560b8ab814a1d05cd6
Way Barrios <waybarrios@gmail.com>
2017-10-30 10:51:00 -0500

    Fixing auth_group fixture

--- pavement.py
+++ pavement.py
@@ -279,6 +279,7 @@ def sync(options):
     sh("python manage.py loaddata sample_admin.json")
     sh("python manage.py loaddata geonode/base/fixtures/default_oauth_apps.json")
     sh("python manage.py loaddata geonode/base/fixtures/initial_data.json")
+    sh("python manage.py loaddata fixtures/default_auth_groups.json")
 
 
 @task
