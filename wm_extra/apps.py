5e61609a1ead3d10b2a3e0de769e1712b4d9a0bf
camp-zju <camp2018@zju.edu.cn>
2018-01-31 22:01:39 +0800

    camp首次提交

--- wm_extra/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class WMExtraConfig(AppConfig):
-    name = 'wm_extra'
-    verbose_name = 'WM Extras'
-
-    def ready(self):
-        import wm_extra.signals
